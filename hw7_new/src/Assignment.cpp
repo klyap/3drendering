d611a4c3e889fae46893259a312b74767e8233e5
klyap <kerleeyap@Kers-MacBook-Pro.local>
2016-12-04 23:31:01 -0800

    camera direcion

--- hw7_new/src/Assignment.cpp
+++ hw7_new/src/Assignment.cpp
@@ -420,7 +420,7 @@ namespace Assignment {
           matrix[0], matrix[1], matrix[2], matrix[3],
           matrix[4], matrix[5], matrix[6], matrix[7],
           matrix[8], matrix[9], matrix[10], matrix[11],
-          matrix[12], matrix[13], matrix[14], matrix[15];
+          matrix[12], matrix[13], matrix[14], 8;
 
         for (int i = 0; i < 16; i++){
           cout << matrix[i] << " ";

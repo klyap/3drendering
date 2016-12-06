6da28e4e9242dc65145c763f16f49b16c1442cba
klyap <kerleeyap@Kers-MacBook-Pro.local>
2016-12-05 23:29:43 -0800

    iter_newton: fixing initial t estimates

--- hw7_new/src/Assignment.cpp
+++ hw7_new/src/Assignment.cpp
@@ -512,11 +512,7 @@ namespace Assignment {
 
         Ray intersection_ray = findIntersection(camera_ray);
 
-        cout << "draw camera_ray origin = " << camera_ray.origin_x << " " <<
-          camera_ray.origin_y << " " << camera_ray.origin_z << endl;
-        cout << "draw camera_ray origin = " << intersection_ray.origin_x << " " <<
-          intersection_ray.origin_y << " " << intersection_ray.origin_z << endl;
-        cout << "draw camera_ray coords = " << endpoint << endl;
+
 
         const float IO_test_color[3] = {0.0, 0.5, 0.5};
         glMaterialfv(GL_FRONT, GL_AMBIENT, IO_test_color);
@@ -529,6 +525,12 @@ namespace Assignment {
         Vector3f endpoint = intersection_ray.getLocation(10.0);
         glVertex3f(endpoint[0], endpoint[1], endpoint[2]);
         glEnd();
+
+        cout << "drew camera_ray origin = " << camera_ray.origin_x << " " <<
+          camera_ray.origin_y << " " << camera_ray.origin_z << endl;
+        cout << "drew intersection_ray origin = " << intersection_ray.origin_x << " " <<
+          intersection_ray.origin_y << " " << intersection_ray.origin_z << endl;
+        cout << "drew intersection_ray end = " << endpoint << endl;
     }
 
     /* Assignment Part B */
